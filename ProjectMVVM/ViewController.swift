6399c57db5c7027452e6144a1c943859471a2f5d
linder3hs <linderhassinger00@gmail.com>
2022-04-22 14:01:42 -0500

    Initial Commit

---
+++ ProjectMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectMVVM
+//
+//  Created by Linder Anderson Hassinger Solano    on 22/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
