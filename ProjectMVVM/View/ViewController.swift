7265dda0446ca4d8382bf4c605bbd42218812bb4
linder3hs <linderhassinger00@gmail.com>
2022-04-22 14:50:07 -0500

    poke template

--- ProjectMVVM/View/ViewController.swift
+++ ProjectMVVM/View/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let pokeViewModel: PokeViewModel = PokeViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pokeViewModel.getDataFromAPI()
+        print("data")
+        print(pokeViewModel.data)
     }
 
 
